26109f3af14dad7cf8c62f569ef0b2e3096cdc05
Brian J. Strout <brianjstrout@gmail.com>
2023-11-27 19:28:40 -0500

    added npm build

--- src/modules/clickListener.js
+++ src/modules/clickListener.js
@@ -1,3 +1,7 @@
+const arrayOfMenuLinks = Array.from(
+  document.querySelectorAll("li.menu__links")
+);
+
 const clicker = (target) => {
   console.log(target);
 
@@ -7,9 +11,30 @@ const clicker = (target) => {
     if (!currentState || currentState === "closed") {
       target.setAttribute("data-state", "opened");
       target.setAttribute("aria-expanded", "true");
+      arrayOfMenuLinks.forEach((link) => {
+        link.classList.add("menu__links_tucked");
+        console.log(link);
+      });
     } else {
       target.setAttribute("data-state", "closed");
       target.setAttribute("aria-expanded", "false");
+      arrayOfMenuLinks.forEach((link) => {
+        link.classList.remove("menu__links_tucked");
+      });
+    }
+  }
+  if (target.classList.contains("menu__links")) {
+    switch (target.id) {
+      case "linkToDogs":
+        console.log("hit");
+        break;
+      case " linkToAction":
+        break;
+
+      case "linkToHelp":
+        break;
+      case "  linkToContact":
+        break;
     }
   }
 };
